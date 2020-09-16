791314792db6cf45d300a3d17c62a62af3591242
Connor <connorskorburg@gmail.com>
2020-09-16 09:52:30 -0500

    added problem solving section notes

---
+++ problemSolving/notes.js
@@ -0,0 +1,31 @@
+/* 
+-- PROBLEM SOLVING NOTES
+
+
+  --UNDERSTAND THE PROBLEM
+    -1. Can I restate the problem in my own words?
+    -2. What are the inputs that go into the problem?
+    -3. What are the outputs that should come from the solution to the problem?
+    -4. Do I have enough info to solve the problem determined from the inputs?
+    -5. How should I label the important pieces of data that are apart of the problem?
+
+  --EXPLORE CONCRETE EXAMPLES
+    -coming up with examples can help you understand the problem better
+    -examples also provide sanity checks that your eventual solution works how it should
+    -1. start w simple examples
+    -2. Progress to more complex examples
+    -3. Explore examples with empty inputs
+    -4. Explore examples with invalid inputs
+
+  --BREAK IT DOWN
+    -
+
+  --SOLVE/SIMPLIFY
+    -
+
+  --LOOK BACK AND REFACTOR
+    -
+
+
+
+*/
