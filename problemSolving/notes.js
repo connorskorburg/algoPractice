7053561975a8701a8d02f7b915195abfa91dfec9
Connor <connorskorburg@gmail.com>
2020-09-19 13:33:53 -0500

    updated problem solving notes

--- problemSolving/notes.js
+++ problemSolving/notes.js
@@ -18,13 +18,24 @@
     -4. Explore examples with invalid inputs
 
   --BREAK IT DOWN
-    -
+    -write down the steps you know you are going to take
+    -write psuedo code out in comments for EVERY Step
 
   --SOLVE/SIMPLIFY
-    -
+    -solve the problem, if you can't... solve a simpler problem
+      -1. find the core difficulty in what you're trying to do
+      -2. temporarily ignore that difficulty 
+      -3. write a simplified solution
+      -4. then incorporate that difficulty back in 
 
   --LOOK BACK AND REFACTOR
-    -
+    -Can you check the result?
+    -Can you derive the result differently?
+    -Can you understand it at a glance?
+    -Can you use the result or method for some other problem?
+    -Can you improve the performance of your solution?
+    -Can you think of other ways to refactor?
+    -How have other people solved this problem?
 
 
 
